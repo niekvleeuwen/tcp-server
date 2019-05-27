7b16e46e7d3439c69e99dbe8201d381b72712cdd
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2019-05-27 18:54:59 +0200

    Added bind, listen and accept

--- server.c
+++ server.c
@@ -1,21 +1,48 @@
 #include<stdio.h>
-#include<sys/socket.h> 
+#include<sys/socket.h>
+#include<arpa/inet.h>	//inet_addr
 
 int main(int argc , char *argv[])
 {
-	int socket_desc;
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-
-    /*
-        Address Family - AF_INET (this is IP version 4)
-        Type - SOCK_STREAM (this means connection oriented TCP protocol)
-        Protocol - 0 [ or IPPROTO_IP This is IP protocol]
-    */
+	int socket_desc , new_socket , c;
+	struct sockaddr_in server , client;
 	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
 	{
-		printf("Could not create socket");
+		printf("Could not create socket\n");
 	}
-	printf("Socket created");
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 ); // je zou hier een integer willen invoeren, maar helaas werken niet alle computers op dezelfde manier met poorten.
+	
+    //Bind
+    if(bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        printf("bind failed\n");
+    }
+    printf("bind done\n");
+	
+	//Listen
+	listen(socket_desc , 3);
+	
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+	{
+		puts("Connection accepted");
+
+	}
+	
+	if (new_socket < 0)
+	{
+		perror("Accept failed");
+		return 1;
+	}
+
 	return 0;
 }
